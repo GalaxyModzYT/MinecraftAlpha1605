aedd5b15cb182ed1bca5a5a5021e5c722e1a84ec
Galaxy Modz <66964223+GalaxyModzYT@users.noreply.github.com>
2023-05-31 19:04:02 -0600

    Add files via upload

---
+++ MCExtension_A1605/src/mcextension_a1605/auth/suspension/SessionUnauthorized.java
@@ -0,0 +1,18 @@
+package mcextension_a1605.auth.suspension;
+
+import javax.swing.*;
+
+public class SessionUnauthorized {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(SessionUnauthorized::showPopup);
+    }
+
+    private static void showPopup() {
+        String message = "Account has been temporarily suspended from Online Play due to violating the gameplay guidelines:\n" +
+                "Session unauthorized\n\n" +
+                "Further violations will lead to a permanent suspension\n\n" +
+                "ID: 2307-gmd7";
+
+        JOptionPane.showMessageDialog(null, message, "Disconnected: Account Suspension", JOptionPane.WARNING_MESSAGE);
+    }
+}
